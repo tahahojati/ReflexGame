9fad7102990259d7b21fc2d4914166810d345557
Taha <taha.hojati@gmail.com>
2018-03-02 16:39:16 -1000

    Added resources, modified layout, starting with Java. I added audio resources, added a setting button, created references in GameActivity.

--- app/src/main/java/io/tpourjalali/reflexgame/GameActivity.java
+++ app/src/main/java/io/tpourjalali/reflexgame/GameActivity.java
@@ -1,10 +1,30 @@
 package io.tpourjalali.reflexgame;
 
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
+import android.media.SoundPool;
 import android.os.Bundle;
+import android.support.annotation.StyleRes;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatImageButton;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
+    private AppCompatImageButton mSettingsImageButton;
+    private TextView mScoreTextView, mHighScoreTextView;
+    private LinearLayout mLivesLayout;
+    private Drawable mHeartDrawable;
+    private Drawable mSpotDrawable;
+    private FrameLayout mGameLayout;
+    private StyleRes mHeartStyle;
+    //End of view element declerations
+
+    private SharedPreferences mPreferences; //we'll save a reference to default sharedpreferences.
+    private int mScore;
+    private SoundPool mSoundPool;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
